352bed2867a65379c479536e2a5faa8d7970788f
EAGEA <emilien.aufauvre@live.fr>
2021-02-23 22:34:33 +0100

    Removed tab.

--- src/superchat/Linker.java
+++ src/superchat/Linker.java
@@ -16,124 +16,124 @@ import java.rmi.*;
  */
 public interface Linker extends Remote 
 {
-	/**
-	 * Add the message from sender to the server history. The server will
-	 * return the date/time the message was sent (thus all the message dates
-	 * will be from the same source i.e. the server).
-	 */
-	String addMessage(String sender, String message) throws RemoteException;
+    /**
+     * Add the message from sender to the server history. The server will
+     * return the date/time the message was sent (thus all the message dates
+     * will be from the same source i.e. the server).
+     */
+    String addMessage(String sender, String message) throws RemoteException;
 
-	/**
-	 * Return true if the user was correctly created on the server side,
-	 * Otherwise return false.
-	 * Reasons why the operation could not be successful:
-	 * - An user with the same name already exists.
-	 */
-	boolean connect(String name) throws RemoteException;
+    /**
+     * Return true if the user was correctly created on the server side,
+     * Otherwise return false.
+     * Reasons why the operation could not be successful:
+     * - An user with the same name already exists.
+     */
+    boolean connect(String name) throws RemoteException;
 
-	/**
-	 * Remove the client identified by name from the list of connected users.
-	 */
-	void disconnect(String name) throws RemoteException;
+    /**
+     * Remove the client identified by name from the list of connected users.
+     */
+    void disconnect(String name) throws RemoteException;
 
-	ArrayList<String> getClientNames() throws RemoteException;
+    ArrayList<String> getClientNames() throws RemoteException;
 
-	ArrayList<Message> getClientMessages() throws RemoteException;
+    ArrayList<Message> getClientMessages() throws RemoteException;
 
-	void setClientMessages(ArrayList<Message> messages) throws RemoteException;
-
-
-	class BasicLinker implements Linker
-	{
-		private final ArrayList<String> mClientNames;
-		private ArrayList<Message> mClientMessages;
-
-		public BasicLinker()
-		{
-			mClientNames = new ArrayList<>();
-			mClientMessages = new ArrayList<>();
-		}
-
-		@Override
-		public String addMessage(String sender, String message) throws RemoteException
-		{
-			// Constants.
-			String DATE_FORMAT = "HH:mm:ss";
-			String time = LocalDateTime.now()
-				.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
-			mClientMessages.add(new Message(time, sender, message));
-
-			return time; 
-		}
-
-		@Override
-		public boolean connect(String name) throws RemoteException 
-		{
-			if (mClientNames.contains(name))
-			{
-				return false; 
-			}
-
-			System.out.println("Client joining: " + name); 
-			mClientNames.add(name);
-			return true;
-		}
-
-		@Override
-		public void disconnect(String name) throws RemoteException
-		{
-			System.out.println("Client exiting: " + name); 
-			mClientNames.remove(name);
-		}
-
-		@Override
-		public ArrayList<String> getClientNames() throws RemoteException
-		{
-			return mClientNames;
-		}
-
-		@Override
-		public ArrayList<Message> getClientMessages() throws RemoteException
-		{
-			return mClientMessages;
-		}
-
-		@Override
-		public void setClientMessages(ArrayList<Message> messages) throws RemoteException
-		{
-			mClientMessages = messages;
-		}
-	}
+    void setClientMessages(ArrayList<Message> messages) throws RemoteException;
 
 
-	class Message implements Serializable
-	{
-		private static final long serialVersionUID = 667363824879925614L;
+    class BasicLinker implements Linker
+    {
+        private final ArrayList<String> mClientNames;
+        private ArrayList<Message> mClientMessages;
 
-		private final String mTime;
-		private final String mSender;
-		private final String mContent;
+        public BasicLinker()
+        {
+            mClientNames = new ArrayList<>();
+            mClientMessages = new ArrayList<>();
+        }
 
-		private Message(String time, String sender, String content)
-		{
-			mTime = time;
-			mSender = sender;
-			mContent = content;
-		}
+        @Override
+        public String addMessage(String sender, String message) throws RemoteException
+        {
+            // Constants.
+            String DATE_FORMAT = "HH:mm:ss";
+            String time = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+            mClientMessages.add(new Message(time, sender, message));
 
-		public String getTime()
-		{
-			return mTime;
-		}
+            return time; 
+        }
 
-		public String getSender()
-		{
-			return mSender;
-		}
+        @Override
+        public boolean connect(String name) throws RemoteException 
+        {
+            if (mClientNames.contains(name))
+            {
+                return false; 
+            }
 
-		public String getContent()
-		{
-			return mContent;
-		}
-	}
+            System.out.println("Client joining: " + name); 
+            mClientNames.add(name);
+            return true;
+        }
+
+        @Override
+        public void disconnect(String name) throws RemoteException
+        {
+            System.out.println("Client exiting: " + name); 
+            mClientNames.remove(name);
+        }
+
+        @Override
+        public ArrayList<String> getClientNames() throws RemoteException
+        {
+            return mClientNames;
+        }
+
+        @Override
+        public ArrayList<Message> getClientMessages() throws RemoteException
+        {
+            return mClientMessages;
+        }
+
+        @Override
+        public void setClientMessages(ArrayList<Message> messages) throws RemoteException
+        {
+            mClientMessages = messages;
+        }
+    }
+
+
+    class Message implements Serializable
+    {
+        private static final long serialVersionUID = 667363824879925614L;
+
+        private final String mTime;
+        private final String mSender;
+        private final String mContent;
+
+        private Message(String time, String sender, String content)
+        {
+            mTime = time;
+            mSender = sender;
+            mContent = content;
+        }
+
+        public String getTime()
+        {
+            return mTime;
+        }
+
+        public String getSender()
+        {
+            return mSender;
+        }
+
+        public String getContent()
+        {
+            return mContent;
+        }
+    }
 }
--- src/superchat/Server.java
+++ src/superchat/Server.java
@@ -20,150 +20,150 @@ import java.rmi.registry.LocateRegistry;
  */
 public class Server 
 {
-	public static void main(String[] args) 
-	{
-		new Server(parseArgs(args));
-	}
+    public static void main(String[] args) 
+    {
+        new Server(parseArgs(args));
+    }
 
-	public static String parseArgs(String[] args)
-	{
-		if (args.length < 1) 
-		{
-			return "localhost";
-		}
+    public static String parseArgs(String[] args)
+    {
+        if (args.length < 1) 
+        {
+            return "localhost";
+        }
 
-		return args[0];
-	}
-	
-	
-	// Path constants.
-	private final String HOME_DIR_PATH = System.getProperty("user.home") 
-		+ File.separator + ".superchat";
-	private final String HISTORY_FILE_PATH = HOME_DIR_PATH + File.separator 
-		+ "history"; 
+        return args[0];
+    }
+    
+    
+    // Path constants.
+    private final String HOME_DIR_PATH = System.getProperty("user.home") 
+        + File.separator + ".superchat";
+    private final String HISTORY_FILE_PATH = HOME_DIR_PATH + File.separator 
+        + "history"; 
 
-	// Linker btw server and clients.
-	private final Linker.BasicLinker mLinker;
+    // Linker btw server and clients.
+    private final Linker.BasicLinker mLinker;
 
-	public Server(String host)
-	{
-		mLinker = new Linker.BasicLinker();
-		// Create/check existence of message history file. 
-		createHomeDir();
-		createHistoryFile();
-		// And retrieve this history (if needed).
-		retrieveMessageHistory();
+    public Server(String host)
+    {
+        mLinker = new Linker.BasicLinker();
+        // Create/check existence of message history file. 
+        createHomeDir();
+        createHistoryFile();
+        // And retrieve this history (if needed).
+        retrieveMessageHistory();
 
-		try 
-		{
-			// Avoid the "rmiregistry & / start rmiregistry" command if on local.
-			if (host.equals("localhost"))
-			{
-				LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-			}
-			// Register the remoted object.
-			Linker linker_stub = (Linker) 
-				UnicastRemoteObject.exportObject(mLinker, 0);
-			Registry registry = LocateRegistry.getRegistry(host);
-			registry.rebind("rmi://server/ConnectService", linker_stub);
-			// Save the messages when exiting.	
-			Runtime.getRuntime().addShutdownHook(new Thread(this::saveMessageHistory));
-		} 
-		catch (Exception e) 
-		{
-			System.err.println("Error: " + e);
-		}
+        try 
+        {
+            // Avoid the "rmiregistry & / start rmiregistry" command if on local.
+            if (host.equals("localhost"))
+            {
+                LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+            }
+            // Register the remoted object.
+            Linker linker_stub = (Linker) 
+                UnicastRemoteObject.exportObject(mLinker, 0);
+            Registry registry = LocateRegistry.getRegistry(host);
+            registry.rebind("rmi://server/ConnectService", linker_stub);
+            // Save the messages when exiting.  
+            Runtime.getRuntime().addShutdownHook(new Thread(this::saveMessageHistory));
+        } 
+        catch (Exception e) 
+        {
+            System.err.println("Error: " + e);
+        }
 
-		// Debug.
-		System.out.println ("Server ready...");
-	}
+        // Debug.
+        System.out.println ("Server ready...");
+    }
 
-	public void retrieveMessageHistory()
-	{
-		try
-		{
-			// Open a stream to the file.
-			ObjectInputStream stream = new ObjectInputStream(
-					new FileInputStream(HISTORY_FILE_PATH));
-			// Then read the messages.
-			@SuppressWarnings("unchecked")
-			ArrayList<Linker.Message> messages = (ArrayList<Linker.Message>) stream.readObject(); 
+    public void retrieveMessageHistory()
+    {
+        try
+        {
+            // Open a stream to the file.
+            ObjectInputStream stream = new ObjectInputStream(
+                    new FileInputStream(HISTORY_FILE_PATH));
+            // Then read the messages.
+            @SuppressWarnings("unchecked")
+            ArrayList<Linker.Message> messages = (ArrayList<Linker.Message>) stream.readObject(); 
 
-			if (messages != null)
-			{
-				mLinker.setClientMessages(messages);
-			}
+            if (messages != null)
+            {
+                mLinker.setClientMessages(messages);
+            }
 
-			stream.close();
-		}
-		catch (EOFException e) 
-		{
-			// If no EOFException then the history file is just empty => normal behavior.
-		}
-		catch (Exception e) 
-		{
-			// If no EOFException then the history file is empty.
-			System.err.println("Error: cannot retrieve messages in the history file."); 
-		}
-	}	
+            stream.close();
+        }
+        catch (EOFException e) 
+        {
+            // If no EOFException then the history file is just empty => normal behavior.
+        }
+        catch (Exception e) 
+        {
+            // If no EOFException then the history file is empty.
+            System.err.println("Error: cannot retrieve messages in the history file."); 
+        }
+    }   
 
-	public void saveMessageHistory()
-	{
-		try
-		{
-			// Open a stream to the file.
-			ObjectOutputStream stream = new ObjectOutputStream(
-					new FileOutputStream(HISTORY_FILE_PATH));
-			// Then write the messages. 
-			stream.writeObject(mLinker.getClientMessages()); 
+    public void saveMessageHistory()
+    {
+        try
+        {
+            // Open a stream to the file.
+            ObjectOutputStream stream = new ObjectOutputStream(
+                    new FileOutputStream(HISTORY_FILE_PATH));
+            // Then write the messages. 
+            stream.writeObject(mLinker.getClientMessages()); 
 
-			stream.close();
-		}
-		catch (Exception e) 
-		{
-			System.err.println("Error: cannot save messages in the history file."); 
-		}
-	}
+            stream.close();
+        }
+        catch (Exception e) 
+        {
+            System.err.println("Error: cannot save messages in the history file."); 
+        }
+    }
 
-	private void createHomeDir()
-	{
-		File homeDir = new File(HOME_DIR_PATH);
+    private void createHomeDir()
+    {
+        File homeDir = new File(HOME_DIR_PATH);
 
-		try
-		{
-			if (! homeDir.exists() && ! homeDir.mkdirs())
-			{
-				System.err.println("Error: cannot create the Superchat home directory " +
-						HOME_DIR_PATH + ".");
-				System.exit(-1);
-			}
-		}
-		catch (Exception e)
-		{
-			System.err.println("Error: cannot create the Superchat home directory " + 
-					HOME_DIR_PATH + "."); 
-			System.exit(-1);
-		}
-	}
+        try
+        {
+            if (! homeDir.exists() && ! homeDir.mkdirs())
+            {
+                System.err.println("Error: cannot create the Superchat home directory " +
+                        HOME_DIR_PATH + ".");
+                System.exit(-1);
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error: cannot create the Superchat home directory " + 
+                    HOME_DIR_PATH + "."); 
+            System.exit(-1);
+        }
+    }
 
-	private void createHistoryFile()
-	{
-		File file = new File(HISTORY_FILE_PATH);
+    private void createHistoryFile()
+    {
+        File file = new File(HISTORY_FILE_PATH);
 
-		try
-		{
-			if (! file.exists() && ! file.createNewFile())
-			{
-				System.err.println("Error: cannot create the history file " +
-						HISTORY_FILE_PATH + ".");
-				System.exit(-1);
-			}
-		}
-		catch (Exception e)
-		{
-			System.err.println("Error: cannot create the history file " + 
-					HISTORY_FILE_PATH + "."); 
-			System.exit(-1);
-		}
-	}
+        try
+        {
+            if (! file.exists() && ! file.createNewFile())
+            {
+                System.err.println("Error: cannot create the history file " +
+                        HISTORY_FILE_PATH + ".");
+                System.exit(-1);
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error: cannot create the history file " + 
+                    HISTORY_FILE_PATH + "."); 
+            System.exit(-1);
+        }
+    }
 }
